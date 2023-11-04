ea6950d209f745e44a8b34107838cbdbb953e6b3
sventro <polymonger@gmail.com>
2023-11-04 13:29:12 +1300

    converted draw world into a class

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 import pygame
 import time
 from world import World, Grid
+from cell import Cell
 
 
-def main():
+def main() -> None:
     pygame.init()
-    screen = pygame.display.set_mode((800, 800))
-    running = True
-    board = World(dimensions=[40, 40], preset=True)
+    screen = pygame.display.set_mode((600, 600))
+    board = World(dimensions=[40, 40], stages=True, preset=False)
 
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         screen.fill("black")
-        draw_world(screen, Grid)
+        Draw(screen=screen, border_size=1, shape="rect")
         board.update_cells()
         pygame.display.flip()
         time.sleep(0.1)
 
 
-def draw_world(
-    screen: pygame.Surface, grid: Grid, rect: bool = True, circle: bool = False
-) -> None:
-    border_size = 1
-    cell_width = screen.get_width() / grid.width
-    cell_height = screen.get_height() / grid.height
-    width = cell_width - border_size
-    height = cell_height - border_size
+class Draw:
+    def __init__(self, screen: pygame.Surface, border_size: int, shape: str) -> None:
+        self.screen = screen
+        self.border_size = border_size
+        self.shape = shape
 
-    for x in range(len(grid.cells)):
-        for y in range(len(grid.cells[x])):
-            cell = grid.cells[x][y]
-            if rect:
-                left = x * cell_width + border_size
-                top = y * cell_height + border_size
-                pygame.draw.rect(screen, cell.status.value, (left, top, width, height))
-            if circle:
-                center = [
-                    x * cell_width + width / 2 + border_size,
-                    y * cell_height + height / 2 + border_size,
-                ]
-                radius = width / 2
-                pygame.draw.circle(screen, cell.status.value, center, radius)
+        self.cell_width = self.screen.get_width() / Grid.width
+        self.cell_height = self.screen.get_height() / Grid.height
+        self.width = self.cell_width - border_size
+        self.height = self.cell_height - border_size
+
+        self.draw_world()
+
+    def draw_world(self) -> None:
+        for x, _ in enumerate(Grid.cells):
+            for y, _ in enumerate(Grid.cells[x]):
+                cell = Grid.cells[x][y]
+                x_position = x * self.cell_width
+                y_position = y * self.cell_height
+                if self.shape == "rect":
+                    self.draw_rect(cell, x_position, y_position)
+                if self.shape == "circle":
+                    self.draw_circle(cell, x_position, y_position)
+
+    def draw_circle(self, cell: Cell, x_pos: int, y_pos: int) -> None:
+        radius = self.width / 2
+        center_x = x_pos + radius + self.border_size
+        center_y = y_pos + radius + self.border_size
+        pygame.draw.circle(self.screen, cell.status.value, (center_x, center_y), radius)
+
+    def draw_rect(self, cell: Cell, x_pos: int, y_pos: int) -> None:
+        left = x_pos + self.border_size
+        top = y_pos + self.border_size
+        rect = (left, top, self.width, self.height)
+        pygame.draw.rect(self.screen, cell.status.value, rect)
 
 
 if __name__ == "__main__":
